8030ac25cab21097feecdd5284a32e30b00fe7fc
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2023-10-10 01:20:17 +0300

    Init Commit

---
+++ mini-opply/vite.config.ts
@@ -0,0 +1,53 @@
+import { AliasOptions, defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+	plugins: [vue(), basicSsl()],
+	resolve: {
+		alias: getAliases(),
+	},
+	server: {
+		host: true,
+		https: true,
+	},
+});
+
+function getAliases(): AliasOptions {
+	return [
+		{
+			find: "@/src",
+			replacement: fileURLToPath(new URL("./src", import.meta.url)),
+		},
+		{
+			find: "@/views",
+			replacement: fileURLToPath(new URL("./src/views", import.meta.url)),
+		},
+		{
+			find: "@/components",
+			replacement: fileURLToPath(new URL("./src/components", import.meta.url)),
+		},
+		{
+			find: "@/types",
+			replacement: fileURLToPath(new URL("./src/types", import.meta.url)),
+		},
+		{
+			find: "@/stores",
+			replacement: fileURLToPath(new URL("./src/stores", import.meta.url)),
+		},
+		{
+			find: "@/api",
+			replacement: fileURLToPath(new URL("./src/api", import.meta.url)),
+		},
+		{
+			find: "@/router",
+			replacement: fileURLToPath(new URL("./src/router", import.meta.url)),
+		},
+		{
+			find: "@/assets",
+			replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
+		},
+	];
+}
