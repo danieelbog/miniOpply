98d9bdda86fc9eced59483639cacd048d3fcdbd7
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2023-10-10 01:25:37 +0300

    - add main routing logic

--- mini-opply/src/router/index.ts
+++ mini-opply/src/router/index.ts
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "@/stores/auth.store";
+import routes from "../views";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+var router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
 
-export default router
+router.beforeEach(async (to, from, next) => {
+	const authStore = useAuthStore();
+	const allowAnonymous = to.matched.some((record) => record.meta.allowAnonymous);
+	const loginQuery = { path: "/login", query: { redirect: to.fullPath } };
+
+	if (!allowAnonymous && !authStore.isAuthenticated()) {
+		authStore.getAuthTokenFromLocalStorage();
+		if (authStore.isAuthenticated()) next();
+		else next(loginQuery);
+	} else next();
+});
+
+export default router;
