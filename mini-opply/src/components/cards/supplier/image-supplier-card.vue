dc2d1c28b21cc59f5ccb0f5574f8f7307e27f056
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2023-10-11 14:03:52 +0300

    - Supplier UI

---
+++ mini-opply/src/components/cards/supplier/image-supplier-card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row justify-content-center w-100 g-0 p-3">
+        <div class="card shadow mb-4">
+            <div class="card-body text-center">
+                <img :src="'https://robohash.org/' + supplier.id" alt="avatar" class="rounded-circle img-fluid"
+                    style="width: 150px;">
+                <h5 class="my-3">{{ supplier.name }}</h5>
+                <p class="text-muted mb-1">{{ supplier.description }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { ISupplier } from '@/src/types/ISupplier';
+
+export default defineComponent({
+    props: {
+        supplier: {
+            type: Object as () => ISupplier,
+            required: true
+        }
+    }
+})
+</script>
--- mini-opply/src/views/Supplier/supplier.vue
+++ mini-opply/src/views/Supplier/supplier.vue
@@ -1,10 +1,55 @@
 <template>
     <div>
-        I am supplier
+        <Loader v-if="!supplier"></Loader>
+        <ErrorHeader v-if="!supplier" :errorMessages="errorMessages" />
+        <ImageSupplierCard v-else :supplier="supplier" />
     </div>
 </template>
-
+  
 <script lang="ts">
-import { defineComponent } from 'vue';
-export default defineComponent({})
+import { defineComponent, onMounted, ref } from 'vue';
+import { ISupplier } from '@/src/types/ISupplier';
+import { api } from '@/src/api';
+
+import Loader from "@/components/layouts/loader/loader.vue";
+import ErrorHeader from '@/components/input/error-header.vue';
+import ImageSupplierCard from '@/components/cards/supplier/image-supplier-card.vue';
+import router from '@/src/router';
+
+export default defineComponent({
+    components: {
+        Loader,
+        ErrorHeader,
+        ImageSupplierCard,
+    },
+    setup() {
+        const supplier = ref<ISupplier | null>(null);
+        const errorMessages = ref<string[]>([]);
+
+        const loadSupplier = async () => {
+            try {
+                const supplierId = router.currentRoute.value.params.supplierId;
+                if (!supplierId) {
+                    throw new Error('Supplier was not found');
+                }
+
+                const response = await api.get(`/api/v1/suppliers/${supplierId}/`);
+                if (response.data) {
+                    supplier.value = response.data;
+                }
+            } catch (error) {
+                const statusText = (error as any).response?.statusText;
+                errorMessages.value = [statusText];
+            }
+        };
+
+        onMounted(loadSupplier);
+
+        return {
+            supplier,
+            errorMessages,
+        };
+    },
+});
 </script>
+  
